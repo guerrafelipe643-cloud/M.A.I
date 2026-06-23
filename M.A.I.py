11bb6a08e99eca683dbed3cc4c88776d96cc87e1
guerrafelipe643-cloud <guerrafelipe643@gmail.com>
2026-06-22 21:43:50 -0300

    Update M.A.I.py

--- M.A.I.py
+++ M.A.I.py
@@ -17,12 +17,23 @@
 # ==============================================================================
 import random, string
 import time
+#ND: neurônio de digíto
+#NP: neurônio de palavra/padrão
+#NS: neurônio de "sintaxe" focado em decisões gerais de reconhecimentos de padrões
+#ta: taxa de aprendizado
+#mem: memória
+#ent: entrada
+#vn: valor neurônio
+#vna: valor atual do neurônio
+#err: erro
 class ND:
+    
     def __init__(self, name):
         self.name = name
         self.mem = {}
         self.vn = 0
-    def detectar(self, ent):
+    
+    def detectar(self, ent): #verifica se ent está em mem
         if ent in self.mem:
             return self.mem[ent], "Conhecido"
         else:
@@ -30,8 +41,8 @@ class ND:
     def aprender(self, entrada):
         ta = 0.1
         vn = self.vn  # começa com o valor atual do neurônio
-        err = vn - entrada
-        while True:
+        err = vn - entrada 
+        while True: #cálculos para aproximação da entrada
             if abs(err) < 1e-2:   
                 break
             err = entrada - vn          
@@ -41,10 +52,10 @@ class ND:
         if vn == entrada:
             self.mem[entrada] = vn     
 class NP:
-    def __init__(self, name):
+    def __init__(self, name): 
         self.name = name
         self.mem = {}
-    def comparar(self, ents):
+    def comparar(self, ents): #verifica se ent é ou não conhecido
         if ents in self.mem:
             return "Conhecido"
         else:
@@ -54,6 +65,7 @@ class NP:
 while True:
     ciclo = 0
     nds = []
+    #menu
     print("=================:MENU M.A.I:=================")
     print("Selecione a opção desejada: ")
     print("1 para sair")
@@ -90,6 +102,7 @@ while True:
             numerond = int(input("Digite o número de ND's: "))
         except ValueError:
             continue
+        #criação dos neurônios
         if "s" in use:
             numnp = numerond // num
             for i in range(numnp):
@@ -102,12 +115,14 @@ while True:
         memoriaglobal = []
         ciclo = 0
         valores_nds = []
+        #ciclo principal
         while ciclo < numerociclos:
             resultado = []
             resultadotodo = []
             valores_nds = []
             conhecidosn = 0
             resultados = []
+            #menu de texto aleatório(apenas para entradas gigantescas como de 2.000.000 de dígitos
             if opcao1 == 1:
                 text = "".join(random.choices(string.ascii_letters + string.digits, k=len(nds)))
             if opcao1 == 2:
@@ -115,24 +130,29 @@ while True:
             if len(text) > len(nds):
                 print(f"Muitos caracteres!, o texto tem {len(text)} caracteres, o máximo é {len(nds)}!")
                 continue
+            #loop para os cáculos corretos
             for i, char in enumerate(text):
+                #transformações de cada digíto do texto
                 if char.isdigit():
                     ent = int(char)
                 else:
                     ent = ord(char)
                 nd = nds[i]
                 valor,status = nd.detectar(ent)
+                #roda função aprender
                 if status == "Novo":
                     nd.aprender(ent)
                 else:
                     conhecidosn += 1
                 valores_nds.append(nd.vn)
             if "s" in use:
+                #fala de quando até quando da lista os nps irão pegar as saídas
                 for i, np in enumerate(nps):
                     inicio = i * num
                     fim = inicio + num
                     ents = tuple(valores_nds[inicio:fim])
                     resultadotodo.append(np.comparar(ents))
+            #prints
             compreensaogeral = ns.comparar(tuple(valores_nds))
             print(f'número de conhecidos (ND) detectados: {conhecidosn}')
             print(f"Esse padrão é: {resultadotodo}")
